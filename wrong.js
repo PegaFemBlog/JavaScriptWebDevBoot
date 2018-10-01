c6067cc9f35564d1f1f8db7b8f83f906f5091543
Rebecca B <39128621+planocodegirl@users.noreply.github.com>
2018-09-30 20:54:27 -0500

    Solution to Pseudocode
    Solution to HTML Pseudocode found in wrong.js

--- wrong.js
+++ wrong.js
@@ -1,4 +1,16 @@
+/*
 var name = prompt("What is your name?");
 console.log("Hello, " + name + "! This is a secret console message. Enjoy being awesome!")
 
 alert("Welcome, " + name + ". This is the best site ever!")
+*/
+
+var firstName = prompt("Please enter your first name.");
+var lastName = prompt("Please enter your last name.");
+alert("Welcome " + firstName + " " + lastName);
+var age = prompt("How old are you?");
+if (age <18) {
+	alert("Sorry, at " + age + " you're too young to vote.");
+} else {
+	alert("Great, at " + age + " you are old enough to vote!");
+}
