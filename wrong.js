23e947f4c3a6320974dc593fa8e9858c44d9d9b1
Rebecca B <39128621+planocodegirl@users.noreply.github.com>
2018-09-30 20:34:12 -0500

    Initial Upload of Practice Files
    Difference between:
    -console.log
    -alert
    -prompt

---
+++ wrong.js
@@ -0,0 +1,4 @@
+var name = prompt("What is your name?");
+console.log("Hello, " + name + "! This is a secret console message. Enjoy being awesome!")
+
+alert("Welcome, " + name + ". This is the best site ever!")
